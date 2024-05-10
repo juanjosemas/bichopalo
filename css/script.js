a5bd4d1342a4455f25237960c1eb4d6b2c642364
juanjosemas <48967497+juanjosemas@users.noreply.github.com>
2024-05-10 16:47:16 +0200

    Add files via upload

--- css/script.js
+++ css/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Obtener el botón de búsqueda
-    var botonBuscar = document.getElementById("buscarBoton");
-    var inputBuscar = document.getElementById("buscarInput");
+    let botonBuscar = document.getElementById("buscarBoton");
+    let inputBuscar = document.getElementById("buscarInput");
 
     // Asociar la función buscar al evento de clic del botón "Buscar"
     botonBuscar.addEventListener("click", function() {
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Función para buscar la palabra ingresada en el input
     function buscar(inputText) {
-        var conceptosU = document.querySelectorAll("#conceptos u"); // Obtener todos los elementos <u> dentro de la lista de conceptos
-        var conceptosP = document.querySelectorAll("#conceptos p"); // Obtener todos los elementos <p> dentro de la lista de conceptos
+        let conceptosU = document.querySelectorAll("#conceptos u"); // Obtener todos los elementos <u> dentro de la lista de conceptos
+        let conceptosP = document.querySelectorAll("#conceptos p"); // Obtener todos los elementos <p> dentro de la lista de conceptos
 
         // Restaurar los colores originales antes de realizar una nueva búsqueda
         conceptosU.forEach(function(concepto) {
@@ -29,25 +29,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Iterar sobre cada elemento <u> para buscar la palabra ingresada
         conceptosU.forEach(function(concepto) {
-            var textoConcepto = concepto.textContent.toLowerCase(); // Obtener el texto dentro del elemento <u> en minúsculas
-            var expresion = new RegExp('\\b' + inputText.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b', 'g'); // Expresión regular para buscar la palabra completa
+            let textoConcepto = concepto.textContent.toLowerCase(); // Obtener el texto dentro del elemento <u> en minúsculas
+            let expresion = new RegExp('\\b' + inputText.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b', 'g'); // Expresión regular para buscar la palabra completa
             if (expresion.test(textoConcepto)) { // Verificar si la palabra coincide con la búsqueda
                 concepto.style.color = "red"; // Resaltar la palabra encontrada en rojo
                 setTimeout(function() {
                     concepto.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Desplazar la página hacia la palabra encontrada
-                }, 100);
+                }, 300);
             }
         });
 
         // Iterar sobre cada elemento <p> para buscar la palabra ingresada
         conceptosP.forEach(function(concepto) {
-            var textoConcepto = concepto.textContent.toLowerCase(); // Obtener el texto dentro del elemento <p> en minúsculas
-            var expresion = new RegExp('\\b' + inputText.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b', 'g'); // Expresión regular para buscar la palabra completa
+            let textoConcepto = concepto.textContent.toLowerCase(); // Obtener el texto dentro del elemento <p> en minúsculas
+            let expresion = new RegExp('\\b' + inputText.toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b', 'g'); // Expresión regular para buscar la palabra completa
             if (expresion.test(textoConcepto)) { // Verificar si la palabra coincide con la búsqueda
                 concepto.style.color = "blue"; // Resaltar la palabra encontrada en azul
                 setTimeout(function() {
                     concepto.scrollIntoView({ behavior: 'smooth', block: 'center' }); // Desplazar la página hacia la palabra encontrada
-                }, 100);
+                }, 300);
             }
         });
 
